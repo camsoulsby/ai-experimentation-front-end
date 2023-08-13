b1ff07d6604f9e78be68f5e4412c786dd3afd214
Cam <cameron.soulsby@gmail.com>
2023-08-13 16:40:47 +1200

    add options to post to different API endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,72 @@ import {
   Container,
   createTheme,
   ThemeProvider,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from "@mui/material";
 import MicIcon from "@mui/icons-material/Mic";
 import StopIcon from "@mui/icons-material/Stop";
 
+const speakText = (text: string) => {
+  const synth = window.speechSynthesis;
+
+  // Get the list of available voices
+  const voices = synth.getVoices();
+
+  // Filter the voices to find one with a male characteristic if available
+  const maleVoice = voices.find(
+    (voice) => voice.name.includes("Male") || voice.lang.includes("Male")
+  );
+
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // If a male voice is available, set it for the utterance
+  if (maleVoice) {
+    utterance.voice = maleVoice;
+  }
+
+  synth.speak(utterance);
+};
+
 const App: React.FC = () => {
+  const [selectedAPIEndpoint, setSelectedAPIEndpoint] = useState(
+    "http://localhost:3001/api/upload-audio"
+  );
   const [isRecording, setIsRecording] = useState(false);
   const [transcribedText, setTranscribedText] = useState("");
+  const [htmlToOutput, setHtmlToOutput] = useState("");
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
     null
   );
+  const [fileToUpload, setFileToUpload] = useState<File | null>(null);
 
   const theme = createTheme({
     palette: {
       primary: {
-        main: "#f44336",
+        main: "#2EE59D ",
+        dark: "#27CC88",
+      },
+      secondary: {
+        main: "#F00",
       },
       background: {
         default: "#222222",
       },
     },
   });
+
+  const handleModelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedAPIEndpoint(event.target.value); // update the selected model
+  };
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      console.log("File selected:", file.name);
+      setFileToUpload(file);
+    }
+  };
 
   const handleRecordingToggle = () => {
     if (isRecording) {
@@ -51,13 +96,10 @@ const App: React.FC = () => {
           const formData = new FormData();
           formData.append("audio", audioBlob);
           console.log("Processing voice input...");
-          const response = await fetch(
-            "http://localhost:3001/api/upload-audio",
-            {
-              method: "POST",
-              body: formData,
-            }
-          );
+          const response = await fetch(selectedAPIEndpoint, {
+            method: "POST",
+            body: formData,
+          });
 
           if (!response.ok) {
             console.error("Upload failed");
@@ -67,6 +109,15 @@ const App: React.FC = () => {
           const data = await response.json();
           const transcribedText = data.text;
           setTranscribedText(transcribedText);
+          speechSynthesis.cancel();
+          // speakText(transcribedText);
+
+          if (
+            selectedAPIEndpoint ===
+            "http://localhost:3001/api/upload-audio-to-code"
+          ) {
+            setHtmlToOutput(transcribedText);
+          }
         };
 
         recorder.start();
@@ -76,6 +127,27 @@ const App: React.FC = () => {
     }
   };
 
+  const handleReviewCall = async () => {
+    console.log("Uploading call for review...");
+    if (fileToUpload) {
+      const formData = new FormData();
+      formData.append("audio", fileToUpload);
+      console.log("Processing call...");
+      const response = await fetch(selectedAPIEndpoint, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        console.error("Upload failed");
+        return;
+      }
+
+      const data = await response.json();
+      const transcribedText = data.text;
+      setTranscribedText(transcribedText);
+    }
+  };
   return (
     <ThemeProvider theme={theme}>
       <Container
@@ -88,20 +160,10 @@ const App: React.FC = () => {
           alignItems: "stretch",
         }}
       >
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={handleRecordingToggle} // Use the new function here
-          endIcon={isRecording ? <StopIcon /> : <MicIcon />}
-          fullWidth
-        >
-          {isRecording ? "Stop Recording" : "Record"}
-        </Button>
-
         <TextField
           id="transcribed-text"
           multiline
-          rows={4}
+          rows={20}
           variant="outlined"
           value={transcribedText} // Set the value to the transcribed text
           fullWidth
@@ -114,6 +176,59 @@ const App: React.FC = () => {
             marginBottom: "10px",
           }}
         />
+        <RadioGroup
+          row
+          value={selectedAPIEndpoint}
+          onChange={handleModelChange}
+        >
+          <FormControlLabel
+            value="http://localhost:3001/api/upload-audio"
+            control={<Radio />}
+            label="GPT-3"
+          />
+          <FormControlLabel
+            value="http://localhost:3001/api/upload-audio-35"
+            control={<Radio />}
+            label="GPT-3.5"
+          />
+          <FormControlLabel
+            value="http://localhost:3001/api/upload-audio-to-code"
+            control={<Radio />}
+            label="Code"
+          />
+          <FormControlLabel
+            value="http://localhost:3001/api/upload-audio-call-review"
+            control={<Radio />}
+            label="Call Review"
+          />
+        </RadioGroup>
+        {selectedAPIEndpoint !==
+        "http://localhost:3001/api/upload-audio-call-review" ? (
+          <Button
+            variant="contained"
+            color={isRecording ? "secondary" : "primary"}
+            onClick={handleRecordingToggle} // Use the new function here
+            endIcon={isRecording ? <StopIcon /> : <MicIcon />}
+            fullWidth
+          >
+            {isRecording ? "Stop Recording" : "Record"}
+          </Button>
+        ) : (
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button variant="contained" color="primary" component="label">
+              Select File
+              <input type="file" hidden onChange={handleFileUpload} />
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleReviewCall}
+            >
+              Review Call
+            </Button>
+          </div>
+        )}
+        <div dangerouslySetInnerHTML={{ __html: htmlToOutput }} />
       </Container>
     </ThemeProvider>
   );
